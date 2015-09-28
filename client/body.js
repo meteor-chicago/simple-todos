7a8efff2886ae50838fc425da74baa05a0562b7f
Ben Strahan <ben@benstrahan.com>
2015-09-28 16:06:40 -0500

    first commit

---
+++ client/body.js
@@ -0,0 +1,37 @@
+// This code only runs on the client
+Template.body.helpers({
+  tasks: function () {
+    if (Session.get("hideCompleted")) {
+      // If hide completed is checked, filter tasks
+      return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+    } else {
+      // Otherwise, return all of the tasks
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+  },
+  hideCompleted: function () {
+    return Session.get("hideCompleted");
+  },
+  incompleteCount: function () {
+    return Tasks.find({checked: {$ne: true}}).count();
+  }
+});
+
+Template.body.events({
+  "submit .new-task": function (event) {
+    // Prevent default browser form submit
+    event.preventDefault();
+
+    // Get value from form element
+    var text = event.target.title.value;
+
+    // Insert a task into the collection
+    Meteor.call("addTask", text);
+
+    // Clear form
+    event.target.title.value = "";
+  },
+  "change .hide-completed input": function (event) {
+    Session.set("hideCompleted", event.target.checked);
+  }
+});
---
+++ server/publications.js
@@ -0,0 +1,10 @@
+// This code only runs on the server
+// Only publish tasks that are public or belong to the current user
+Meteor.publish("tasks", function () {
+  return Tasks.find({
+    $or: [
+      { private: {$ne: true} },
+      { owner: this.userId }
+    ]
+  });
+});
